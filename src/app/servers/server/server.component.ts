704dea3866102427e937a40b795a4dfd2788ede3
TheoCaesar <theocaesar7@gmail.com>
2024-08-12 05:27:22 +0100

    dynamic navigation with params

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,11 +10,17 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
+  serverId: number;
+  actRoute = inject(ActivatedRoute)
 
   constructor(private serversService: ServersService) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // this.server = this.serversService.getServer(1);
+    const subscr = this.actRoute.params.subscribe((response: Params)=> {
+      this.serverId = +response.id //precede with + to convert to int
+      this.server = this.serversService.getServer(this.serverId);
+    });  
   }
 
 }
